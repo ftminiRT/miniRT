df616c1bf6f8819aa559cf79ea064f654ab600fd
Teddy Coeffet <tcoeffet@f2r7s7.clusters.42paris.fr>
2025-08-29 14:35:53 +0200

    add value "max" to moebius parsing

--- src/parsing/init_obj_bonus.c
+++ src/parsing/init_obj_bonus.c
@@ -99,20 +99,20 @@ int	init_boloid(char **args, t_env *rt, t_objtype type)
 
 	arg_len = count_arg(args);
 	if (arg_len != 5 && arg_len != 6)
-		return (printf("92"), 1);
+		return (1);
 	new = create_object(rt);
 	if (!new)
-		return (printf("95"), 1);
+		return (1);
 	new->type = type;
 	if (str_to_vec3(&new->pt, args[1]))
-		return (printf("98"), 1);
+		return (1);
 	if (str_to_vec3(&new->n, args[2]) || check_norm(new->n))
-		return (printf("100"), 1);
+		return (1);
 	if (!str_is_double(args[3]))
 		return (1);
 	new->scal = ft_atod(args[3]);
 	if (str_to_colors(&new->color, args[4]))
-		return (printf("103"), 1);
+		return (1);
 	if (arg_len == 6)
 		return (set_shine(new, args[5]));
 	return (0);
@@ -124,23 +124,24 @@ int	init_moebius(char **args, t_env *rt)
 	size_t	arg_len;
 
 	arg_len = count_arg(args);
-	if (arg_len != 6 && arg_len != 7)
-		return (printf("113"), 1);
+	if (arg_len != 7 && arg_len != 8)
+		return (1);
 	new = create_object(rt);
 	if (!new)
-		return (printf("116"), 1);
+		return (1);
 	new->type = OT_MOEB;
 	if (str_to_vec3(&new->pt, args[1]))
-		return (printf("119"), 1);
-	if (!str_is_double(args[2]) || !str_is_double(args[3]))
-		return (printf("121"), 1);
+		return (1);
+	if (!str_is_double(args[2]) || !str_is_double(args[3]) || !str_is_double(args[4]))
+		return (1);
 	new->scal = ft_atod(args[2]);
 	new->scal2 = ft_atod(args[3]);
-	if (str_to_vec3(&new->n, args[4]) || check_norm(new->n))
-		return (printf("125"), 1);
-	if (str_to_colors(&new->color, args[5]))
-		return (printf("127"), 1);
-	if (arg_len == 7)
-		return (set_shine(new, args[6]));
+	new->max = ft_atod(args[4]);
+	if (str_to_vec3(&new->n, args[5]) || check_norm(new->n))
+		return (1);	
+	if (str_to_colors(&new->color, args[6]))
+		return (1);
+	if (arg_len == 8)
+		return (set_shine(new, args[7]));
 	return (0);
 }

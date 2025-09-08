2a3923adae91a24712e3673bacf3804555e3a055
beauman <beauman@le.con>
2025-09-08 21:33:11 +0200

    fix make

--- src/ui/build_pane/ui_build_dft.c
+++ src/ui/build_pane/ui_build_dft.c
@@ -16,34 +16,33 @@ int	build_pane_dft(void *rt, t_uipane *current)
 {
 	t_cam	*c;
 	t_light	*a;
-	t_items *i;
 	int		step = 5;
 
-	i = &current->itms;
+	(void)rt;
 	c = current->cam;
 	a = current->light;
-	if (add_btn(setb(&a->color.r, -step, pt(106, 92), BTNSZ), i) ||
-		add_btn(setb(&a->color.g, -step, pt(106, 122), BTNSZ), i) ||
-		add_btn(setb(&a->color.b, -step, pt(106, 152), BTNSZ), i) ||
-		add_btn(setb(&a->brightness, -step, pt(106, 182), BTNSZ), i) ||
-		add_btn(setb(&a->color.r, step, pt(136, 92), BTNSZ), i) ||
-		add_btn(setb(&a->color.g, step, pt(136, 122), BTNSZ), i) ||
-		add_btn(setb(&a->color.b, step, pt(136, 152), BTNSZ), i) ||
-		add_btn(setb(&a->brightness, step, pt(136, 182), BTNSZ), i) ||
-		add_btn(setb(&c->pos.x, -step, pt(106, 242), BTNSZ), i) ||
-		add_btn(setb(&c->pos.y, -step, pt(106, 272), BTNSZ), i) ||
-		add_btn(setb(&c->pos.z, -step, pt(106, 302), BTNSZ), i) ||
-		add_btn(setb(&c->dir.x, -step, pt(106, 332), BTNSZ), i) ||
-		add_btn(setb(&c->dir.y, -step, pt(106, 362), BTNSZ), i) ||
-		add_btn(setb(&c->dir.z, -step, pt(106, 392), BTNSZ), i) ||
-		add_btn(setb(&c->fov, -step, pt(106, 422), BTNSZ), i) ||
-		add_btn(setb(&c->pos.x, step, pt(136, 242), BTNSZ), i) ||
-		add_btn(setb(&c->pos.y, step, pt(136, 272), BTNSZ), i) ||
-		add_btn(setb(&c->pos.z, step, pt(136, 302), BTNSZ), i) ||
-		add_btn(setb(&c->dir.x, step, pt(136, 332), BTNSZ), i) ||
-		add_btn(setb(&c->dir.y, step, pt(136, 362), BTNSZ), i) ||
-		add_btn(setb(&c->dir.z, step, pt(136, 392), BTNSZ), i) ||
-		add_btn(setb(&c->fov, step, pt(136, 422), BTNSZ), i))
+	if (add_btn(setb(&a->color.r, -step, pt(106, 92), BTNSZ), current) ||
+		add_btn(setb(&a->color.g, -step, pt(106, 122), BTNSZ), current) ||
+		add_btn(setb(&a->color.b, -step, pt(106, 152), BTNSZ), current) ||
+		add_btn(setb(&a->brightness, -step, pt(106, 182), BTNSZ), current) ||
+		add_btn(setb(&a->color.r, step, pt(136, 92), BTNSZ), current) ||
+		add_btn(setb(&a->color.g, step, pt(136, 122), BTNSZ), current) ||
+		add_btn(setb(&a->color.b, step, pt(136, 152), BTNSZ), current) ||
+		add_btn(setb(&a->brightness, step, pt(136, 182), BTNSZ), current) ||
+		add_btn(setb(&c->pos.x, -step, pt(106, 242), BTNSZ), current) ||
+		add_btn(setb(&c->pos.y, -step, pt(106, 272), BTNSZ), current) ||
+		add_btn(setb(&c->pos.z, -step, pt(106, 302), BTNSZ), current) ||
+		add_btn(setb(&c->dir.x, -step, pt(106, 332), BTNSZ), current) ||
+		add_btn(setb(&c->dir.y, -step, pt(106, 362), BTNSZ), current) ||
+		add_btn(setb(&c->dir.z, -step, pt(106, 392), BTNSZ), current) ||
+		add_btn(setb(&c->fov, -step, pt(106, 422), BTNSZ), current) ||
+		add_btn(setb(&c->pos.x, step, pt(136, 242), BTNSZ), current) ||
+		add_btn(setb(&c->pos.y, step, pt(136, 272), BTNSZ), current) ||
+		add_btn(setb(&c->pos.z, step, pt(136, 302), BTNSZ), current) ||
+		add_btn(setb(&c->dir.x, step, pt(136, 332), BTNSZ), current) ||
+		add_btn(setb(&c->dir.y, step, pt(136, 362), BTNSZ), current) ||
+		add_btn(setb(&c->dir.z, step, pt(136, 392), BTNSZ), current) ||
+		add_btn(setb(&c->fov, step, pt(136, 422), BTNSZ), current))
 		return (1);
 	return (0);
 }

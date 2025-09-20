8c1183d2dc93f554e0cb283960c1b8f6795da141
tcoeffet <tcoeffet@student.42.fr>
2025-09-20 15:28:22 +0200

    add cone norm

--- src/basics_norm.c
+++ src/basics_norm.c
@@ -128,7 +128,18 @@ t_vec3	cylinder_normal(t_obj *obj, t_vec3 hit_point)
 
 t_vec3 cone_normal(t_obj *obj, t_vec3 hit_point)
 {
-	(void) hit_point;
-	(void) obj;
-	return ((t_vec3){1,0,0});
+	t_vec3	tmp;
+	t_vec3	ret;
+	double	angle;
+	double	k;
+
+	angle = (obj->scal * M_PI) / 180.0;
+	k = 1.0 + (tan(angle) * tan(angle));
+
+	tmp = vec3_sub(hit_point, obj->pt);
+	ret.x = 2 * tmp.x;
+	ret.y = 2 * tmp.y;
+	ret.z = 2 * (tmp.z) - 2 * k * vec3_dot(tmp, obj->n) * obj->n.z;
+	vec3_normalize(&ret);
+	return (ret);
 }

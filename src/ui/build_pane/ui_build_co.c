fdc462df57118b499217a753de077b1417b324e4
tcoeffet <tcoeffet@student.42.fr>
2025-09-20 22:14:35 +0200

    fixed a typo in build_co.c

--- src/ui/build_pane/ui_build_co.c
+++ src/ui/build_pane/ui_build_co.c
@@ -36,7 +36,7 @@ int	build_pane_co(void *rt, t_uipane *current)
 				|| add_btn(setb((t_btn_data){&o->scal2, -STEP_HEI,
 				null_vec(), UIT_SCL_BTN}, pt(106, 392), btn_sz()), current)
 				|| add_btn(setb((t_btn_data){&o->scal2, STEP_HEI,
-				null_vec(), UIT_SCL_BTN}, pt(136, 392), btn_sz()), current);
+				null_vec(), UIT_SCL_BTN}, pt(136, 392), btn_sz()), current))
 		return (1);
 	return (0);
 }
